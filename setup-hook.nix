9e0a327667f05b49172e7ef6aebb82c43ab107b1
Alexandria P. <alxpettit@gmail.com>
2024-10-28 19:49:28 -0600

    ok

--- setup-hook.nix
+++ setup-hook.nix
@@ -5,43 +5,40 @@
   linkFarm,
   fetchurl,
   writeShellScript,
-}:
-
-{
+}: {
   lockfile,
-  overrides ? (final: prev: { }),
-}:
+  overrides ? (final: prev: {}),
+}: let
+  initialDeps =
+    builtins.mapAttrs (
+      _: dep: let
+        throwSystem = throw "${dep.name} unsupported system: ${stdenv.system}";
+        src = dep.systems.${stdenv.system} or throwSystem;
+      in
+        stdenv.mkDerivation {
+          pname = dep.name;
+          version = dep.version;
+          src = fetchurl src;
+          sourceRoot = ".";
 
-let
-  initialDeps = builtins.mapAttrs (
-    _: dep:
-    let
-      throwSystem = throw "${dep.name} unsupported system: ${stdenv.system}";
-      src = dep.systems.${stdenv.system} or throwSystem;
-    in
-    stdenv.mkDerivation {
-      pname = dep.name;
-      version = dep.version;
-      src = fetchurl src;
-      sourceRoot = ".";
+          env.MANIFEST = dep.manifest;
+          buildPhase = ''
+            mkdir -p $out
+            mv * $out
+            echo $MANIFEST > $out/.piopm
+          '';
 
-      env.MANIFEST = dep.manifest;
-      buildPhase = ''
-        mkdir -p $out
-        mv * $out
-        echo $MANIFEST > $out/.piopm
-      '';
-
-      passthru = {
-        inherit (dep) manifest;
-        installPath = dep.install_path;
-        mutableInstall = false;
-      };
-    }
-  ) (builtins.fromJSON (builtins.readFile lockfile)).dependencies;
+          passthru = {
+            inherit (dep) manifest;
+            installPath = dep.install_path;
+            mutableInstall = false;
+          };
+        }
+    )
+    (builtins.fromJSON (builtins.readFile lockfile)).dependencies;
   finalDeps = initialDeps // (overrides finalDeps initialDeps);
 in
-makeSetupHook
+  makeSetupHook
   {
     name = "platformio-setup-hook";
     passthru = {
@@ -51,37 +48,36 @@ makeSetupHook
   (
     let
       # derived from `linkFarm`
-      linkCommands = lib.mapAttrsToList (
-        _: drv:
-        let
-          dest = "$PLATFORMIO_CORE_DIR/${drv.passthru.installPath}";
-        in
-        ''
-          mkdir -p "$(dirname "${dest}")"
-          ${
-            if drv.passthru.mutableInstall then
+      linkCommands =
+        lib.mapAttrsToList (
+          _: drv: let
+            dest = "$PLATFORMIO_CORE_DIR/${drv.passthru.installPath}";
+          in ''
+            mkdir -p "$(dirname "${dest}")"
+            ${
+              if drv.passthru.mutableInstall
+              then ''
+                cp -Lr "${drv}" "${dest}"
+                chmod -R +w "${dest}"
               ''
-                cp -Lr ${drv} ${dest}
-                chmod -R +w ${dest}
-              ''
-            else
-              ''ln -s ${drv} ${dest}''
-          }
-        ''
-      ) finalDeps;
+              else ''ln -s "${drv}" "${dest}"''
+            }
+          ''
+        )
+        finalDeps;
     in
-    writeShellScript "platformio-setup-hook.sh" ''
-      _platformioSetupHook() {
-        # top-level directory must be writable by PlatformIO
-        export PLATFORMIO_CORE_DIR=./core-dir
-        mkdir -p $PLATFORMIO_CORE_DIR
-        ${lib.concatStrings linkCommands}
+      writeShellScript "platformio-setup-hook.sh" ''
+        _platformioSetupHook() {
+          # top-level directory must be writable by PlatformIO
+          export PLATFORMIO_CORE_DIR=./core-dir
+          mkdir -p $PLATFORMIO_CORE_DIR
+          ${lib.concatStrings linkCommands}
+        }
+        preConfigureHooks+=(_platformioSetupHook)
+      ''
+      // {
+        passthru = {
+          inherit finalDeps;
+        };
       }
-      preConfigureHooks+=(_platformioSetupHook)
-    ''
-    // {
-      passthru = {
-        inherit finalDeps;
-      };
-    }
   )
